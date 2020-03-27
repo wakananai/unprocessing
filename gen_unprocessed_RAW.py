305d9511c361e90398b843c0fa9e6a5c2774155e
Yu An Chen <r07922076@cml26.csie.ntu.edu.tw>
2020-03-27 15:50:37 +0800

    renew

--- gen_unprocessed_RAW.py
+++ gen_unprocessed_RAW.py
@@ -8,9 +8,19 @@ import os
 import numpy as np
 
 import pickle
+import argparse
 
-IMG_DIR='/tmp3/r07922076/coco/coco2017/train2017'
-OUT_DIR='/tmp3/r07922076/unprocessed_RAW/train2017'
+# IMG_DIR='/tmp3/r07922076/coco/coco2017/val2017'
+# OUT_DIR='/tmp3/r07922076/unprocessed_COCO2017_RAW/val2017'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ev', type=int, required=True)
+args = parser.parse_args()
+
+
+subdir = f'decay_{args.ev}'
+IMG_DIR= f'/tmp3/r07922076/PASCALRAW_original/{subdir}/jpg'
+OUT_DIR= f'/tmp3/r07922076/unprocessed_PASCALRAW_RAW/{subdir}'
 
 if not os.path.exists(OUT_DIR):
     os.makedirs(OUT_DIR)
@@ -19,7 +29,13 @@ if not os.path.exists(OUT_DIR):
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 
+
+input_image = tf.placeholder(tf.float32, shape=[None, None, 3])
+
+un_raw, meta = unprocess(input_image)
+
 sess = tf.Session(config=config)
+
 with sess.as_default():
     for imgpath in tqdm(sorted(glob.glob(os.path.join(IMG_DIR, '*.jpg')))):
         img = cv2.imread(imgpath)
@@ -34,18 +50,13 @@ with sess.as_default():
 
         plane = np.zeros((h,w,3))
         plane[:img.shape[0],:img.shape[1],:] = img[:,:,:]
-        
-        plane = tf.cast(plane, tf.float32) / 255.0
-        un, metadata = unprocess(plane)
-        
-        copy_metadata = dict()
-        for key in metadata:
-            copy_metadata[key] = metadata[key].eval()
-        
+        plane = plane.astype(np.float32) / 255.0
+        un, metadata = sess.run([un_raw, meta], feed_dict={input_image: plane})
+
         path_raw = os.path.join(OUT_DIR, os.path.basename(imgpath).replace('jpg','pkl'))
-        
+
         with open(path_raw, 'wb') as pf:
             content = dict()
-            content['raw'] = un.eval()
-            content['metadata'] = copy_metadata
+            content['raw'] = un
+            content['metadata'] = metadata
             pickle.dump(content, pf)
